5eafc2dc58068bf26050cf814b733cac7fce2140
Jarrett Revels <jarrettrevels@gmail.com>
2020-12-29 19:05:49 -0500

    add missing line

--- src/TimeSpans.jl
+++ src/TimeSpans.jl
@@ -11,10 +11,10 @@ export TimeSpan, start, stop
 """
     TimeSpan(start, stop)
 
-Return `TimeSpan(Nanosecond(start), Nanosecond(stop))`.
+Return `TimeSpan(Nanosecond(start), Nanosecond(stop))` representing the interval `[start, stop)`.
 
 If `start == stop`, a single `Nanosecond` is added to `stop` since `stop` is an exclusive
-upper bound and TimeSpans.jl operations only support up to nanosecond precision anyway.
+upper bound and TimeSpan operations only generally support up to nanosecond precision anyway.
 
 The benefit of this type over e.g. `Nanosecond(start):Nanosecond(1):Nanosecond(stop)` is
 that instances of this type are guaranteed to obey `TimeSpans.start(x) < TimeSpans.stop(x)`
