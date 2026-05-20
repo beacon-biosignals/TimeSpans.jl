934eab0401dc1fe72c978cad248c9b7df6d3071a
Zachary Chetchavat <zachary.chetchavat@gmail.com>
2026-05-19 20:55:48 -0500

    Update src/TimeSpans.jl
    Co-authored-by: Phillip Alday <me@phillipalday.com>

--- src/TimeSpans.jl
+++ src/TimeSpans.jl
@@ -409,7 +409,8 @@ Returns a timespan that consists of the intersection of two timespans.
 function intersect_spans(span_1, span_2)
     overlaps(span_1, span_2) || throw(ArgumentError("provided spans must overlap"))
 
-    return TimeSpan(maximum(start.([span_1, span_2])), minimum(stop.([span_1, span_2])))
+    spans = [span_1, span_2]
+    return TimeSpan(maximum(start, spans), minimum(stop, spans))
 end
 
 #####
