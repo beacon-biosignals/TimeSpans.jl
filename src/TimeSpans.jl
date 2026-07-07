5c8ca44ae519f16974ffada562dd3e89c11d0a1a
Zachary Chetchavat <zachary.chetchavat@beacon.bio>
2026-07-07 14:51:24 -0500

    add typing to intersect

--- src/TimeSpans.jl
+++ src/TimeSpans.jl
@@ -400,13 +400,13 @@ function invert_spans(spans, parent_span)
 end
 
 """
-    intersect(span_1, span_2)
+    intersect(a::TimeSpan, b::TimeSpan)
 
 Returns a timespan that consists of the intersection of two timespans.
 
 Throws an `ArgumentError` if there is not overlap between the spans.
 """
-function intersect(a, b)
+function intersect(a::TimeSpan, b::TimeSpan)
     overlaps(a, b) || throw(ArgumentError("provided spans must overlap"))
     return TimeSpan(max(start(a), start(b)), min(stop(a), stop(b)))
 end
